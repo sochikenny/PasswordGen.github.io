48dba62eb3e127047d10a20ccbccb63853b5e82b
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-02-29 16:14:31 -0500

    more stuff. still not done

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  //below are my additions//
+function generatePassword() {
   var passwordlength = prompt("How many characters will you input in your password? Enter a number between 8 and 128");
   var passwordchar = {
       lowercase: "abcdefghijklmnopqrstuvwxyz",
@@ -19,38 +15,57 @@ function writePassword() {
   passwordchar.numeric = passwordchar.numeric.split("");
   passwordchar.specialchar = passwordchar.specialchar.split("");
 
-  var promptlower = prompt ("Select a lowercase letter")
-  var promptupper = prompt ("Select an uppercase letter")
-  var promptnumeric = prompt ("Select a numeric value between 0 and 9")
-  var promptspecialchar = prompt ("Select a special character")
-
-  //for (var i = 0; i < passwordchar.lowercase.length; i++) 
-    //if (promptlower === passwordchar.lowercase[i])
-    //result = 
+  var promptlower = confirm ("Do you want a lowercase letter?")
+  var promptupper = confirm ("Do you want an uppercase letter?")
+  var promptnumeric = confirm ("Do you want a numeric value?")
+  var promptspecialchar = confirm (" Do you want a special charater? For ex: !#$%&'()*+,-./:;<=>?@[\]^_`{|}~  ")
   
-  //for (var j = 0; j < passwordchar.uppercase.length; j++) 
-    //if (promptupper === passwordchar.uppercase[i])
-    //result = 
+
+  var result = "";
+  for (var i = 0; i < Number(passwordlength); i++) {
+    if (promptlower){
+      var lc = passwordchar.lowercase[Math.floor(Math.random()* passwordchar.lowercase.length)];
+       result += lc
+    }
+
+    else if (promptupper) {
+      var uc = passwordchar.uppercase[Math.floor(Math.random()* passwordchar.uppercase.length)];
+      result += uc
+    }
+
+    else if (promptnumeric) {
+      var num = passwordchar.numeric[Math.floor(Math.random()* passwordchar.numeric.length)];
+      result += num
+    }
+
+    else if (promptspecialchar === passwordchar.specialchar[i]){
+      var sc = passwordchar.specialchar[Math.floor(Math.random()* passwordchar.specialchar.length)];
+      result += sc
+    }
+  }
+      return result
+
+
+
+
+
+}
+
+
+
+
+  //for (var j = 0; j < result.length; j+=4)
+  
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.innerText = password;
 
   
-    //for (var k = 0; k < passwordchar.numeric.length; k++) 
-      //if (promptnumeric === passwordchar.numeric[i])
-      //result = 
+  
 
-    
-     //for (var l = 0; l < passwordchar.specialchar.length; l++) 
-      //if (promptspecialchar === passwordchar.specialchar[i])
-      //result = 
-    
-     
-    
-      //}
-            
-            return;
-            //return password?? not sure (stuck here..NEED HELP!!!)
-            // well, I just need help in general
-      
-      
   
 
   //below are instructors addtions
